a3169a62bb7393fe162fef75c8ea79397b7bf7cc
edgar sibrian <edg4r168@gmail.com>
2024-05-22 14:47:36 -0600

    Crear instancia de Entrenador y mostrar datos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
+        var entrenador = new Entrenador(1, "Juan", "Perez", 19);
 
+        System.out.println("ID: " + entrenador.getId() + ", Nombre: " + entrenador.getNombre()
+                + ", Apellido: " + entrenador.getApellidos() + ", Edad: " + entrenador.getEdad());
     }
 }
